5f249ef0448f1cae3e11e7efb5d8eed186556d4b
João Pedro Carvalho <peugomescarvalho2@gmail.com>
2021-06-21 15:42:03 -0300

    add statusCount

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,12 +1,27 @@
+const Job = require('../model/Job')
+const Profile = require('../model/Profile')
+const JobUtils = require('../utils/JobUtils')
+
+
 module.exports = {
 	index(req, res) {
 		const jobs = Job.get()
 		const profile = Profile.get()
 
+		let statusCount = {
+			progress: 0,
+			done: 0,
+			total: jobs.length
+		}
+
 		const updatedJobs = jobs.map((job) => {
 			//ajustes no job
 			const remaining = JobUtils.remainingDays(job)
 			const status = remaining <= 0 ? 'done' : 'progress'
+
+			// Estou somando 1 dependendo do valor que a constante status recebe. Se for 'done' soma 1 na minha propriedade done do statusCount assim como se for 'progress'.
+			statusCount[status] += 1
+
 
 			return {
 				...job,
@@ -16,7 +31,7 @@ module.exports = {
 			}
 		})
 
-		return res.render("index", { jobs: updatedJobs })
+		return res.render("index", { jobs: updatedJobs, profile, statusCount })
 	}
 }
 
