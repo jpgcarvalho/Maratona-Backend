982f9112c625d03b6eb57feaa3fc140b6a98d1b8
João Pedro Carvalho <peugomescarvalho2@gmail.com>
2021-06-22 16:15:51 -0300

    add db configuration

---
+++ src/db/config.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3')
+const { open } = require('sqlite')
+
+
+// O método open() tem que estar dentro de uma função para ser usado, por isso estamos exportando uma arrow function e dentro dela colocamos ele
+module.exports = () => {
+	open({
+		filename: './database.sqlite',
+		driver: sqlite3.Database
+	})
+}
